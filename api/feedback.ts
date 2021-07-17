540e5b835880433512e99bf940a9956fe4afa0e8
Mike Nikles <mikenikles@gmail.com>
2021-07-17 21:16:27 +0000

    Satisfy the TS compiler.

--- api/feedback.ts
+++ api/feedback.ts
@@ -1,4 +1,4 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default (request: VercelRequest, response: VercelResponse) => {
   const { name = 'World' } = request.query;
